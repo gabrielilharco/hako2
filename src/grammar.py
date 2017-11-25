f00a3bfb7eda4f90f9c6e6156b3fd0319822b41f
gabrielilharco <gabriel.ilharco@gmail.com>
2017-11-25 10:04:41 -0200

    Adding grammar for random sentence generation

---
+++ src/grammar.py
@@ -0,0 +1,169 @@
+import argparse
+from sets import Set
+import random
+
+grammar = {
+	'Sentenca': [
+		['Complemento', 'Sentenca_1'],
+		['Sentenca_1'],
+		['Sentenca_2'],
+	],
+	'Sentenca_1': [
+		['Sujeito', 'Verbo_Transitivo_Temporal', 'Objeto'],
+		['Objeto', 'Sujeito', 'Verbo_Transitivo_Temporal'],
+		['Sujeito', 'Verbo_Intransitivo_Temporal'],
+		['Verbo_Intransitivo_Temporal', 'Sujeito'],
+	],
+	'Sentenca_2': [
+		['Sujeito', 'Verbo_Transitivo_Atemporal', 'Objeto'],
+		['Objeto', 'Sujeito', 'Verbo_Transitivo_Atemporal'],
+		['Sujeito', 'Verbo_Intransitivo_Atemporal'],
+		['Verbo_Intransitivo_Atemporal', 'Sujeito'],
+	],
+	'Complemento': [
+		['Hoje'],
+		['Ontem'],
+		['Amanha'],
+		['Passado'],
+		['Futuro'],
+		['Segunda-feira'],
+		['Terca-feira'],
+		['Quarta-feira'],
+		['Quinta-feira'],
+		['Sexta-feira'],
+		['Sabado'],
+		['Domingo'],
+	],
+	'Sujeito': [
+		['Eu'],
+		['Voce'],
+		['El@'],
+		['Sujeito_1'],
+		['Sujeito_1', 'Modificador_Sujeito'],
+	],
+	'Sujeito_1': [
+		['Homem'],
+		['Mulher'],
+		['Crianca'],
+	],
+	'Modificador_Sujeito': [
+		['Velh@'],
+		['Amig@'],
+		['Filh@'],
+		['Bonit@'],
+		['Fei@'],
+		['Legal'],
+		['Gord@'],
+		['Magr@'],
+		['Alt@'],
+		['Calm@']
+	],
+	'Verbo_Intransitivo_Temporal': [
+		['Estudar'],
+		['Trabalhar'],
+		['Cantar'],
+		['Dormir'],
+		['Comemorar'],
+		['Namorar'],
+		['Acordar'],
+		['Brincar'],
+		['Cozinhar'],
+		['Esperar'],
+		['Chorar'],
+		['Rir'],
+		['Viajar'],
+	],
+	'Verbo_Intransitivo_Atemporal': [
+		['Crescer'],
+	],
+	'Verbo_Transitivo_Temporal': [
+		['Comprar'],
+		['Vender'],
+		['Encontrar'],
+		['Mostrar'],
+		['Procurar'],
+		['Ganhar'],
+	],
+	'Verbo_Transitivo_Atemporal': [
+		['Gostar'],
+		['Gostar-Nao'],
+		['Amar'],
+		['Ter'],
+	],
+	'Objeto': [
+		['Substantivo'],
+		['Substantivo', 'Adjetivo'],
+	],
+	'Substantivo': [
+		['Livro'],
+		['Casa'],
+		['Carro'],
+		['Mochila'],
+		['Televisao'],
+		['Pedra'],
+		['Trem'],
+		['Barco'],
+		['Computador'],
+		['Celular'],
+		['Chapeu'],
+		['Tenis'],
+		['Oculos'],
+		['Relogio'],
+		['Camisa'],
+	],
+	'Adjetivo': [
+		['Vermelh@'],
+		['Verde'],
+		['Azul'],
+		['Amarel@'],
+		['Pret@'],
+		['Branc@'],
+		['Bonit@'],
+		['Fei@']
+	],
+}
+
+terminals = Set()
+
+def populate_terminals():
+	terminals = Set()
+	for key, val in grammar.iteritems():
+		for word_list in val:
+			for word in word_list:
+				if word not in grammar:
+					terminals.add(word)
+	return terminals
+
+def generate_random_sentence():
+	sentence = ['Sentenca']
+	idx = 0
+	while idx < len(sentence):
+		if sentence[idx] in terminals:
+			idx += 1
+		else:
+			choices = grammar[sentence[idx]]
+			choice = random.choice(choices)
+			sentence = sentence[:idx] + choice + sentence[idx+1:]
+	sentence = " ".join([word.upper() for word in sentence])
+	print sentence
+	return sentence
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description='Grammar utils')
+	parser.add_argument('--print_atoms', action='store_true',
+                      help='Print atoms of the grammar')
+  	parser.add_argument('--num_sentences', type=int, default=0,
+                      help='The number of random sentences to generate.')
+
+	args = parser.parse_args()
+
+	terminals = populate_terminals()
+
+  	if args.print_atoms:
+  		for terminal in sorted(terminals):
+			print terminal
+		print '-----------------'
+		print'There are', len(terminals), 'terminals'
+
+	for i in range(args.num_sentences):
+		generate_random_sentence()
